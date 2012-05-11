787d60690960d9eaca45a753a6adc7dab19f20f4
Carl Masak <cmasak@gmail.com>
2012-05-11 16:19:08 +0200

    refactor
    Now we look at the big picture, and unify things a bit.
    The if statements go away.
    
    In the demo when I did this, I used built-in refactoring
    menu options in Eclipse. Use them, they're great.

--- demo-2/src/se/edument/roman/RomanConverter.java
+++ demo-2/src/se/edument/roman/RomanConverter.java
@@ -2,10 +2,13 @@ package se.edument.roman;
 
 public class RomanConverter {
 	public String toRoman(int number) {
-		if (number == 3)
-			return "III";
-		if (number == 2)
-			return "II";
-		return "I";
+		return times(number, "I");
+	}
+
+	private String times(int number, String string) {
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < number; i++)
+			sb.append(string);
+		return sb.toString();
 	}
 }
