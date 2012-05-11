a43965ff74f19b90dd465660784ff6fcf903c0ee
Carl Masak <cmasak@gmail.com>
2012-05-11 19:32:56 +0200

    dependency injection step 1
    Turn local variables using the concrete class into
    an instance field.

--- demo-3/src/se/edument/walkdetector/WalkDetector.java
+++ demo-3/src/se/edument/walkdetector/WalkDetector.java
@@ -3,17 +3,16 @@ package se.edument.walkdetector;
 public class WalkDetector {
 	private Location startLocation;
 	private boolean isWalking = false;
+	private RealGPS gps = new RealGPS();
 	
 	public void startWalking() {
 		isWalking = true;
-		RealGPS gps = new RealGPS();
 		startLocation = gps.getCurrentLocation();
 	}
 	
 	public float getWalkingDistance() {
 		if (!isWalking)
 			throw new IllegalStateException("getWalkingDistance() called before startWalking()");
-		RealGPS gps = new RealGPS();
 		Location currentLocation = gps.getCurrentLocation();
 		return currentLocation.distanceTo(startLocation);
 	}
