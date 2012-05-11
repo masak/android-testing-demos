2e188e327518ad37d28207d61ae019971f4ceefd
Carl Masak <cmasak@gmail.com>
2012-05-11 14:32:18 +0200

    added OnItemClickListener to the list
    It displays a Toast message.
    Note the general principle of how listeners work: you derive from a
    listener of some kind, and provide your own methods inline. If your
    listener implementation becomes large or is used in many places,
    one can name it and place it somewhere else.

--- demo-1/src/se/edument/recipebook/RecipeListActivity.java
+++ demo-1/src/se/edument/recipebook/RecipeListActivity.java
@@ -2,8 +2,13 @@ package se.edument.recipebook;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RecipeListActivity extends ListActivity {
     @Override
@@ -14,6 +19,14 @@ public class RecipeListActivity extends ListActivity {
 
     	  ListView lv = getListView();
     	  lv.setTextFilterEnabled(true);
+    	  
+    	  lv.setOnItemClickListener(new OnItemClickListener() {
+    		  public void onItemClick(AdapterView<?> parent, View view,
+    				  int position, long id) {
+
+    			  Toast.makeText(RecipeListActivity.this, ((TextView)view).getText(), 3);
+    		  }
+    	  });
     }
     
     static final String[] RECIPES = new String[] {
